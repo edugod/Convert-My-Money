159032b19b8ad3fd7b7e809b7b78b100c8d1a3b5
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-10-16 14:10:53 -0300

    learning errors in api

--- test-api.js
+++ test-api.js
@@ -8,6 +8,7 @@ const url = `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/Cotac
 axios
     .get(url)
     .then( res => console.log(res.data.value[0].cotacaoVenda))
+    .catch( err => console.log(err)) //pra caso dar erro, mostrar o parâmetro error
 
 //aqui eu consegui pegar a cotacao do dia que eu fiz lá no api do bacen, daqui a pouco vouaprender a pegar dados atualizados
 //praisso eu vou criar um módulo dentro do lib 'api.bcb.js'
